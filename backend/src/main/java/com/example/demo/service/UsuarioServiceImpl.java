4bc88724b39205d91b9c91f0a3090b5c688d3aa3
David <davidvt04@gmail.com>
2026-02-23 11:37:16 +0100

    feat: :sparkles: Añadida entidad abstracta Usuario, junto con su controlador, servicio y repositorio
    He añadido la entidad Usuario de acuerdo al modelo conceptual. Además, he dejado preparados ya el controlador, servicio y repositorio
    
    Refs #6

---
+++ backend/src/main/java/com/example/demo/service/UsuarioServiceImpl.java
@@ -0,0 +1,19 @@
+package com.example.demo.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.demo.repository.UsuarioRepository;
+
+@Service
+@Transactional
+public class UsuarioServiceImpl implements UsuarioService {
+
+    private final UsuarioRepository usuarioRepository;
+
+    @Autowired
+    public UsuarioServiceImpl(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+}
