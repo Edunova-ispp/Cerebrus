f937b9defb9ddbdf5ff7b1db264a1b626a429917
David <davidvt04@gmail.com>
2026-02-23 13:44:09 +0100

    feat: :sparkles: Creada entidad Pregunta junto con el controlador, servicio y repositorios asociados
    He creado la entidad Pregunta junto con el controlador, servicio y repositorios asociados
    
    Refs #6

---
+++ backend/src/main/java/com/example/demo/controller/PreguntaController.java
@@ -0,0 +1,21 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.service.PreguntaService;
+
+@RestController
+@RequestMapping("/api/preguntas")
+@CrossOrigin(origins = "*")
+public class PreguntaController {
+
+    private final PreguntaService preguntaService;
+
+    @Autowired
+    public PreguntaController(PreguntaService preguntaService) {
+        this.preguntaService = preguntaService;
+    }
+}
