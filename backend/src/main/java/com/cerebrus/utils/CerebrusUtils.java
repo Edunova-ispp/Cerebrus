0c85b1065d353f5998aaa40f68c365859d69821f
David <davidvt04@gmail.com>
2026-02-23 15:43:58 +0100

    feat: :sparkles: Añadida clase CerebrusUtils para métodos de ayuda en el backend
    He añadido la clase CerebrusUtils para métodos de ayuda en el backend
    
    Refs #6

---
+++ backend/src/main/java/com/cerebrus/utils/CerebrusUtils.java
@@ -0,0 +1,18 @@
+package com.cerebrus.utils;
+
+import java.util.Collection;
+
+public class CerebrusUtils {
+
+    public static String generateUniqueCode(){
+        //TODO: Implementar lógica para generar un código único de 7 caracteres alfanuméricos (mayúsculas y números)
+        String codigoEjemplo ="DAVID123";
+        return codigoEjemplo;
+    }
+
+    public static <T> Collection<T> shuffleCollection(Collection<T> collection) {
+        //TODO: Implementar lógica para mezclar aleatoriamente los elementos de la colección
+        return collection;
+    }
+    
+}
