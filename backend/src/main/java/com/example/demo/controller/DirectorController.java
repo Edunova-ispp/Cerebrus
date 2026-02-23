e86e0390a25cebd03fbc9933f9c4780984646e07
David <davidvt04@gmail.com>
2026-02-23 11:46:42 +0100

    feat: :sparkles: Añadida entidad Director, junto con su controlador, servicio y repositorio
    He añadido la entidad Director de acuerdo al modelo conceptual. Además, he creado el controlador, servicio y repositorio listos para trabajar
    
    Refs #6

---
+++ backend/src/main/java/com/example/demo/controller/DirectorController.java
@@ -0,0 +1,21 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.service.DirectorService;
+
+@RestController
+@RequestMapping("/api/directores")
+@CrossOrigin(origins = "*")
+public class DirectorController {
+
+    private final DirectorService directorService;
+
+    @Autowired
+    public DirectorController(DirectorService directorService) {
+        this.directorService = directorService;
+    }
+}
