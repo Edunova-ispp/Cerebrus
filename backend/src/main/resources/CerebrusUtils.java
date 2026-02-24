0b3a73c9ae10f0227d73db4fe478b9b6a61cd232
Mpino <marperdom@alum.us.es>
2026-02-24 15:47:46 +0100

    creado metodo para generar código único

--- backend/src/main/resources/CerebrusUtils.java
+++ backend/src/main/resources/CerebrusUtils.java
@@ -1,24 +1,18 @@
-package com.cerebrus.utils;
-
 import java.security.SecureRandom;
-import java.util.Collection;
 
-public class CerebrusUtils {
+public final class CerebrusUtils {
+
     private static final char[] ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".toCharArray();
     private static final int CODE_LENGTH = 7;
     private static final SecureRandom RNG = new SecureRandom();
 
-    public static String generateUniqueCode(){
+    private CerebrusUtils() {}
+
+    public static String generateUniqueCode() {
         char[] code = new char[CODE_LENGTH];
         for (int i = 0; i < CODE_LENGTH; i++) {
             code[i] = ALPHABET[RNG.nextInt(ALPHABET.length)];
         }
         return new String(code);
     }
-
-    public static <T> Collection<T> shuffleCollection(Collection<T> collection) {
-        //TODO: Implementar lógica para mezclar aleatoriamente los elementos de la colección
-        return collection;
-    }
-    
 }
