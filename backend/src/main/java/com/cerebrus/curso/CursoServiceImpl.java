b0c30cf6706a395818ebf85b1bb8e64b55ec1212
Mtolgon <mantolgon@alum.us.es>
2026-02-25 10:27:40 +0100

    feature: service for listado cursos

--- backend/src/main/java/com/cerebrus/curso/CursoServiceImpl.java
+++ backend/src/main/java/com/cerebrus/curso/CursoServiceImpl.java
@@ -14,4 +14,24 @@ public class CursoServiceImpl implements CursoService {
     public CursoServiceImpl(CursoRepository cursoRepository) {
         this.cursoRepository = cursoRepository;
     }
+
+    public List<Curso> ObtenerCursosUsuarioLogueado() {
+        //Esta funcion devuleve una lista con todos los cursos del usuario logueado, 
+        // si el usuario es un maestro devuelve los cursos que ha creado, 
+        // si el usuario es un alumno devuelve los cursos a los que se ha inscrito 
+        // y que son visibles.
+
+
+        //TODO:obtener el usuario logueado, se ha asumido un metodo obtenerUsuarioLogueado().
+       Usuario usuario = obtenerUsuarioLogueado(); 
+        if (usuario instanceof Maestro) {
+             return cursoRepository.findByMaestroId(usuario.getId());
+        } else if (usuario instanceof Alumno) {
+             return cursoRepository.findByAlumnoId(usuario.getId());
+        }
+
+       
+    }
+
+
 }
