e522c0b49a994ab06b16de361d63b34e5edbae5d
Mtolgon <mantolgon@alum.us.es>
2026-02-25 11:17:03 +0100

    feature: service for detalles curso

--- backend/src/main/java/com/cerebrus/curso/CursoServiceImpl.java
+++ backend/src/main/java/com/cerebrus/curso/CursoServiceImpl.java
@@ -46,6 +46,38 @@ public class CursoServiceImpl implements CursoService {
 
        
     }
+     public List<String> obtenerDetallesCurso(Long id) {
+        //Esta funcion devuelve una lista de strings con los detalles(titulo, descripcion, imagen y/o codigo) 
+        // de un curso específico, 
+        // pero solo si el usuario logueado tiene permiso para verlo.
+        //En el caso de los maestros, solo pueden ver los detalles de los cursos que ellos mismos han creado.
+        //En el caso de los alumnos, solo pueden ver los detalles de los cursos a los que están inscritos y que además son visibles.
+        Curso curso=cursoRepository.findByID(id);
+        if(curso==null){
+            throw new RuntimeException("404 Not Found");
+        }
+        //TODO:obtener el usuario logueado, se ha asumido un metodo obtenerUsuarioLogueado().
+        Usuario usuario = usuarioService.findCurrentUser(); 
+        if(usuario instanceof Maestro){
+            if(curso.getMaestro().getId()!=usuario.getId()){
+                throw new RuntimeException("403 Forbidden");
+            }else {
+                return List.of(curso.getTitulo(), curso.getDescripcion(), curso.getImagen(), curso.getCodigo());
+            }
+
+
+        }else if(usuario instanceof Alumno){
+           List<Curso> cursosAlumno=cursoRepository.findByAlumnoId(usuario.getId());
+           
+            if(curso.getVisibilidad()==false||!cursosAlumno.contains(curso)){
+                throw new RuntimeException("403 Forbidden");
+                }else{
+                    return List.of(curso.getTitulo(), curso.getDescripcion(), curso.getImagen());
+                }
+        }else{
+            throw new RuntimeException("403 Forbidden");
+}
+     }
 
 
 }
