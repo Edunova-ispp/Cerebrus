79b53a5c906d1a856427c58778a124bda1599093
juagonluc1 <juagonluc1@alum.us.es>
2026-02-27 12:18:07 +0100

    atributos, getter, setter

--- backend/src/main/java/com/cerebrus/actividadalumno/ActividadAlumno.java
+++ backend/src/main/java/com/cerebrus/actividadalumno/ActividadAlumno.java
@@ -4,9 +4,13 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.cglib.core.Local;
+
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -15,6 +19,10 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+
 import com.cerebrus.actividad.Actividad;
 import com.cerebrus.usuario.Alumno;
 import com.cerebrus.respuestaalumno.RespuestaAlumno;
@@ -35,6 +43,29 @@ public class ActividadAlumno {
 
     @Column(nullable = false)
     private LocalDate fecha;
+
+    @Column
+    private LocalDate inicio = LocalDate.ofEpochDay(0);
+
+    @Column
+    private LocalDate acabada = LocalDate.ofEpochDay(0);
+
+    @Column(nullable = false)
+    private Integer numAbandonos = 0;
+
+    @Column (nullable = false)
+    @Min(0)
+    @Max(10)
+    private Integer nota;
+
+    @Transient
+    private Integer numRepeticiones;
+
+    @Transient
+    private Integer numFallos;
+
+    @Transient
+    private EstadoActividad estadoActividad;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "alumno_id", nullable = false)
@@ -85,12 +116,70 @@ public class ActividadAlumno {
         this.puntuacion = puntuacion;
     }
 
+    public Integer getNota(){
+        return nota;
+    }
+
+    public void setNota(Integer nota){
+        this.nota = nota;
+    }
+
     public LocalDate getFecha() {
         return fecha;
     }
 
     public void setFecha(LocalDate fecha) {
         this.fecha = fecha;
+    }
+
+    public LocalDate getInicio(){
+        return inicio;
+    }
+
+    public void setInicio(LocalDate inicio){
+        this.inicio=inicio;
+    }
+
+    public LocalDate getAcabada(){
+        return acabada;
+    }
+
+    public void setAcabada(LocalDate acabada){
+        this.acabada=acabada;
+    }
+
+    public Integer getNumAbandonos(){
+        return numAbandonos;
+    }
+
+    public void setNumAbandonos(Integer numAbandonos){
+        this.numAbandonos = numAbandonos;
+    }
+
+    public Integer getNumRepeticiones(){
+        return respuestasAlumno == null ? 0 : respuestasAlumno.size();
+    }
+
+    public Integer getNumFallos(){
+        Integer result;
+        if (respuestasAlumno==null) {
+            result=0;
+        } else {
+            result = (int) respuestasAlumno.stream().filter(r->Boolean.FALSE.equals(r.getCorrecta())).count();
+        }
+        return result;
+    }
+
+    public EstadoActividad getEstadoActividad(){
+        EstadoActividad result;
+        if(getNumRepeticiones() > 0 && respuestasAlumno.get(respuestasAlumno.size()).getCorrecta().equals(Boolean.TRUE)){
+            result = EstadoActividad.TERMINADA;
+        } else if (inicio != null && !(inicio.equals(LocalDate.ofEpochDay(0)))){
+            result = EstadoActividad.EMPEZADA;
+        } else {
+            result = EstadoActividad.SIN_EMPEZAR;
+        }
+        return result;
     }
 
     public Alumno getAlumno() {
@@ -124,6 +213,9 @@ public class ActividadAlumno {
                 ", tiempo=" + tiempo +
                 ", puntuacion=" + puntuacion +
                 ", fecha=" + fecha +
+                ", fecha_inicio=" + inicio + 
+                ", fecha_fin=" + acabada +
+
                 '}';
     }
 }
