42b0d39e79c4baa1ebcc8f25d4dc5fed55b2449d
rafseggom <rafseggom@alum.us.es>
2026-02-27 18:04:23 +0100

    fix(CursoController):  remove unnecessary @CrossOrigin annotation
    * Eliminated the `@CrossOrigin` annotation to streamline the controller.
    * This change helps in maintaining a cleaner codebase.
    * No funciona cuando hay credenciales, por eso lo he quitado
    * Se centraliza todo en securityConfig

--- backend/src/main/java/com/cerebrus/curso/CursoController.java
+++ backend/src/main/java/com/cerebrus/curso/CursoController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,7 +18,6 @@ import jakarta.validation.constraints.NotBlank;
 import java.util.List;
 @RestController
 @RequestMapping("/api/cursos")
-@CrossOrigin(origins = "*")
 public class CursoController {
 
     private final CursoServiceImpl cursoService;
