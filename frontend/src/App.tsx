115738b52522fc754409f7931a38cd289c65b838
rafseggom <rafseggom@alum.us.es>
2026-02-27 04:26:40 +0100

    feat:  add route for MisCursos page
    * Introduced a new route for the `MisCursos` component.
    * Enhances navigation for users to access their courses.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import DeployTesting from "./pages/deployTesting/DeployTesting";
 import LandingPage from "./pages/landingPage/LandingPage";
 import InfoPage from "./pages/infoPage/InfoPage";
+import MisCursos from "./pages/misCursos/MisCursos";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Route path="/infoAlumnos"   element={<InfoPage userType="alumno" />} />
       <Route path="/infoProfesores" element={<InfoPage userType="profesor" />} />
       <Route path="/infoDueños"    element={<InfoPage userType="dueno" />} />
+      <Route path="/misCursos"     element={<MisCursos />} />
     </Routes>
   );
 }
