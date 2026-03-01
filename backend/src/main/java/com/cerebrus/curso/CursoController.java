d34b4df8dfdd37dc37fe88fa29fcf53428d7e4da
Mtolgon <mantolgon@alum.us.es>
2026-03-01 23:19:15 +0100

    bug:arreglo merge

--- backend/src/main/java/com/cerebrus/curso/CursoController.java
+++ backend/src/main/java/com/cerebrus/curso/CursoController.java
@@ -1,7 +1,6 @@
 package com.cerebrus.curso;
 
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -14,8 +13,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.cerebrus.usuario.Alumno;
 
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
