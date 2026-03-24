a6d81a696d406d7a73247a8dc560c0ae6bd755ed
Fernando-Partal <fpgsfc@gmail.com>
2026-03-24 22:23:33 +0100

    fix: otro fix

--- frontend/src/pages/crearActividad/crearActividad.tsx
+++ frontend/src/pages/crearActividad/crearActividad.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import NavbarMisCursos from '../../components/NavbarMisCursos/NavbarMisCursos';
 import { ClasificacionForm } from './ClasificacionForm';
 import './crearActividad.css';
