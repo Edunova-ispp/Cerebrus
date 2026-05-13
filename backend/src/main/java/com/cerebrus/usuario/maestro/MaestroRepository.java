2a976056eabb4ae8f6be7ae99ca0c75fd1982449
ivaferlim <ivaferlijob@gmail.com>
2026-05-13 17:42:24 +0200

    fix: Rutas y añadir mas usuarios de los que deberias poder

--- backend/src/main/java/com/cerebrus/usuario/maestro/MaestroRepository.java
+++ backend/src/main/java/com/cerebrus/usuario/maestro/MaestroRepository.java
@@ -6,4 +6,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface MaestroRepository extends JpaRepository<Maestro, Long> {
 
+    long countByOrganizacionId(Long organizacionId);
+
 }
